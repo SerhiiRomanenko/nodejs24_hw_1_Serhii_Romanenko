2b96831ba97f8807982a62dad3d1279f249a2a19
Serhii <SerhiiRomanenko13@gmail.com>
2024-01-30 18:57:11 +0200

    Add to Logger some Configs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const logger = require("./utils/logger")("main");
 
-logger.info("the script is running!");
-logger.warn("the script has some warn!");
-logger.error("the script has some troubles!");
+logger.info("the script is running! (info)");
+logger.warn("the script has some warn! (warn)");
+logger.error("the script has some troubles! (error)");
--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,21 @@
+const colors = require("colors/safe");
+
+const config = require("config");
+config.COLORS_ENABLED == 1 ? colors.enable() : colors.disable();
+
 console.log("Logger connected correctly!");
 
 function logger(module) {
-  return {
+  let loggerMethods = {
     info: (...allIncomingThings) =>
-      console.info(`${module}:`, ...allIncomingThings),
+      config.LOG_LEVEL === "info" &&
+      console.info(colors.bgGreen(`${module}:`), ...allIncomingThings),
     warn: (...allIncomingThings) =>
-      console.warn(`${module}:`, ...allIncomingThings),
+      config.LOG_LEVEL !== "error" &&
+      console.warn(colors.bgYellow(`${module}:`), ...allIncomingThings),
     error: (...allIncomingThings) =>
-      console.error(`${module}:`, ...allIncomingThings),
+      console.error(colors.bgRed(`${module}:`), ...allIncomingThings),
   };
+  return loggerMethods;
 }
 module.exports = logger;
