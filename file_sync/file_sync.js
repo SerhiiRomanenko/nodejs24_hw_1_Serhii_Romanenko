32cfe25b3843799ed6faa1291b58bacc4102f54c
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2024-02-02 13:20:02 +0200

    added the ability to copy files and folders WITHOUT RECURSION

--- file_sync/file_sync.js
+++ file_sync/file_sync.js
@@ -11,16 +11,22 @@ function file_sync() {
       for (const item of dataSource) {
         const sourceFilePath = `./source/${item}`;
         if (dataTarget.includes(item)) {
+          logger.warn(`File(folder) "${item}" already exists`); // log the info about existing files/folders
         } else if (
           !dataTarget.includes(item) &&
           (await fs.promises.stat(`./source/${item}`)).isDirectory() === false
         ) {
+          let dataFile = await fsAsync.readFile(sourceFilePath, "utf8"); // read files from ./source
+          await fsAsync.writeFile(`./target/${item}`, dataFile, "utf-8"); // write files to ./target
+          logger.info(`File "${item}" was copied successfully`); // log the info about coping of files
         } else if (
           (
             !dataTarget.includes(item) &&
             (await fs.promises.stat(`./source/${item}`))
           ).isDirectory() === true
         ) {
+          await fsAsync.mkdir(`./target/${item}`); // write folders to ./target
+          logger.info(`Folder "${item}" was created successfully`); // log the info about creating of folders
         }
       }
     },
