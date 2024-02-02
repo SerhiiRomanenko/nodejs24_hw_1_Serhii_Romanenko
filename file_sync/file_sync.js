08a4f5e8a67c5e444a152dfe2f15382721019ca6
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2024-02-02 13:15:10 +0200

    created the base of the module file_sync

---
+++ file_sync/file_sync.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const fsAsync = require("fs/promises");
+const logger = require("../utils/logger")("file_sync");
+
+function file_sync() {
+  return {};
+}
+
+module.exports = file_sync;
