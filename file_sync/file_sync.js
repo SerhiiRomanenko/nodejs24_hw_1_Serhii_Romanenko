190a08fdc3b2a44295a152f55d1af8dfc0699168
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2024-02-02 13:16:34 +0200

    geted arrays of files in folders

--- file_sync/file_sync.js
+++ file_sync/file_sync.js
@@ -3,7 +3,12 @@ const fsAsync = require("fs/promises");
 const logger = require("../utils/logger")("file_sync");
 
 function file_sync() {
-  return {};
+  return {
+    start: async function syncFiles() {
+      let dataSource = await fsAsync.readdir("./source"); // get array of files in ./source
+      let dataTarget = await fsAsync.readdir("./target"); // get array of files in ./target
+    },
+  };
 }
 
 module.exports = file_sync;
