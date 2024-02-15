81bccf6ead058243de0f7f971784ccaeaf6cd599
Serhii <SerhiiRomanenko13@gmail.com>
2024-02-15 22:15:38 +0200

    maked response for req.url === /healthcheck and get request

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ const port = 3000;
 server.listen(port);
 
 server.on("listening", () => console.log(`waiting on ${port} port`));
+
+server.on("request", (req, res) => {
+  if (req.url !== "/favicon.ico") {
+    if (req.url === `/healthcheck` && req.method === "GET") {
+      res.writeHead(200);
+      res.write("healthcheck passed");
+    }
+  }
+  res.end();
+});
